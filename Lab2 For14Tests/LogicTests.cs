57af128af1f79bb005f7afdd4af1e46a388ffff9
ValletOr <mtvac11@gmail.com>
2021-10-04 22:55:47 +0800

    Добавил тесты

---
+++ Lab2 For14Tests/LogicTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab2_For14;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_For14.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void NoMatch()
+        {
+            int[] numbers = new int[] { 1, 2, 2, 3, 3, 3, 2, 2 };
+            int n = 4;
+            Assert.AreEqual(Logic.Execution(numbers, n), "Пары не найдены");
+        }
+
+        [TestMethod()]
+        public void Match()
+        {
+            int[] numbers = new int[] { 1, 2, 2, 3, 3, 3, 2, 2 };
+            int n = 3;
+            Assert.AreEqual(Logic.Execution(numbers, n), "Номера первой найденной 3-ки: 4 5 6 ");
+        }
+
+        [TestMethod()]
+        public void FewMatches()
+        {
+            int[] numbers = new int[] { 1, 2, 2, 3, 3, 3, 2, 2 };
+            int n = 2;
+            Assert.AreEqual(Logic.Execution(numbers, n), "Номера первой найденной 2-ки: 2 3 ");
+        }
+    }
+}
